b9c19cb4799a78fc8d8def28d34ed0632a982cf8
Oleksandr <oleksandr@Oleksandrs-MacBook-Pro.local>
2024-09-10 20:31:10 +0300

    feat: add footer; add header

---
+++ src/components/header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LogoImg from '../../assets/logo.png';
+
+const Header = () => {
+    return (
+        <div>
+            <img src = {LogoImg}/>
+        </div>
+    );
+};
+
+export default Header;
