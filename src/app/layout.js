4551eca95a0c06f237fafb06d0151190b6c67368
romandydyk <romandydyk34@gmail.com>
2024-09-13 16:31:06 +0300

    change fonts

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,18 @@
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+const eUkraineMediumSans = localFont({
+    src: "./fonts/e-Ukraine-Medium.ttf",
+    variable: "--font-e-ukraine-medium",
+    weight: "500",
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+
+const eUkraineRegularSans = localFont({
+    src: "./fonts/e-Ukraine-Regular.ttf",
+    variable: "--font-e-ukraine-regular",
+    weight: "400",
 });
+
 
 export const metadata = {
   title: "Create Next App",
@@ -21,7 +23,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${eUkraineMediumSans.variable} ${eUkraineRegularSans.variable} antialiased`}
       >
         {children}
       </body>
