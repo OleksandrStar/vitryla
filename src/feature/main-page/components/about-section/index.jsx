e94070b6d5c9cb0c52c466f2a658fe41da4c2e12
Oleksandr <oleksandr@Oleksandrs-MacBook-Pro.local>
2024-09-14 13:25:23 +0300

    fix: fix font family

---
+++ src/feature/main-page/components/about-section/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AboutSection = () => {
+  return <div className="container">Вчитися не цікаво</div>;
+};
+
+export default AboutSection;
--- src/feature/main-page/index.js
+++ src/feature/main-page/index.js
@@ -1,30 +1,13 @@
-'use client';
-import React from 'react';
-import Footer from "../../components/footer";
-import './style.css'
-import { motion } from 'framer-motion';
-import Header from "@/components/header";
+"use client";
+import React from "react";
+import AboutSection from "@/feature/main-page/components/about-section";
 
 const MainPage = () => {
-
-
-    return (
-        <div>
-            {/*<div className='h-[1000px] bg-red' style={{height: '1000px'}}></div>*/}
-            {/*fsdfds*/}
-            {/*<motion.div*/}
-            {/*    initial={{opacity: 0, transform: 'translateX(100px)'}}*/}
-            {/*    whileInView={{opacity: 1, transform: 'translateX(0)'}}*/}
-            {/*    viewport={{once: false, amount: 0.7, }}*/}
-            {/*>*/}
-            {/*    <div>*/}
-
-            {/*    I appear when in view*/}
-            {/*    </div>*/}
-            {/*</motion.div>*/}
-            {/*<div className='h-[1000px]' style={{height: '1000px'}}></div>*/}
-        </div>
-    );
+  return (
+    <div>
+      <AboutSection />
+    </div>
+  );
 };
 
 export default MainPage;
