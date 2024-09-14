e13ef404dd529ff4f39a60a04118d09112fdeb3f
Oleksandr <oleksandr@Oleksandrs-MacBook-Pro.local>
2024-09-14 13:48:21 +0300

    feat: add animations to title sections

--- src/features/main-page/index.js
+++ src/features/main-page/index.js
@@ -1,4 +1,5 @@
 "use client";
+import { motion } from "framer-motion";
 import React from "react";
 import AboutSection from "@/features/main-page/components/about-section";
 
@@ -6,12 +7,26 @@ const MainPage = () => {
   return (
     <div>
       <div className="font-bold container w-[750px] mx-auto leading-[30px] my-10">
-        <div className="flex flex-col gap-2">
-          <div className="text-6xl">ВЧИТИСЯ</div>
-          <div className="text-6xl">НЕ</div>
-          <div className="text-6xl">ЦІКАВО</div>
-        </div>
-        <div className="text-right mt-5">якщо не знаєш навіщо</div>
+        <motion.div
+          initial={{ opacity: 0, transform: "translateX(-200px)" }}
+          whileInView={{ opacity: 1, transform: "translateX(0)" }}
+          viewport={{ once: true, amount: 0.7 }}
+        >
+          <div className="flex flex-col gap-2">
+            <div className="text-6xl">ВЧИТИСЯ</div>
+            <div className="text-6xl">НЕ</div>
+            <div className="text-6xl">ЦІКАВО</div>
+          </div>
+        </motion.div>
+
+        <motion.div
+          initial={{ opacity: 0, transform: "translateX(200px)" }}
+          whileInView={{ opacity: 1, transform: "translateX(0)" }}
+          transition={{ delay: 0.5, duration: 0.3 }}
+          viewport={{ once: true, amount: 0.7 }}
+        >
+          <div className="text-right mt-5">якщо не знаєш навіщо</div>
+        </motion.div>
       </div>
       <AboutSection />
     </div>
