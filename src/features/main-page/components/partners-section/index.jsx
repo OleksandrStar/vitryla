882e81c7adf86e6aa13b79f134fa2e51ee8e20b3
Oleksandr <oleksandr@Oleksandrs-MacBook-Pro.local>
2024-09-16 12:27:22 +0300

    fix: fix

--- src/features/main-page/components/partners-section/index.jsx
+++ src/features/main-page/components/partners-section/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { partnersList } from "@/features/main-page/components/partners-section/partners-utils";
 import Image from "next/image";
+import clsx from "clsx";
 
 const PartnersSection = () => {
   return (
     <div className="container px-10 lg:px-20 mt-[40px]">
-      <h5 className="text-[25px] md:text-[35px] text-center lg:text-[55px]">
+      <h5 className="font-bold text-[25px] md:text-[35px] text-center lg:text-[55px]">
         Партнери
       </h5>
       <div className="flex flex-wrap mt-5 md:mt-10 w-full">
@@ -13,7 +14,7 @@ const PartnersSection = () => {
           return partnerData.href ? (
             <a
               href={partnerData.href}
-              className="md:w-1/3 w-1/2 relative"
+              className="md:w-1/3 w-1/2 relative xl:p-[30px]"
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -30,7 +31,13 @@ const PartnersSection = () => {
 
 const PartnerItem = ({ partnerData }) => {
   return (
-    <div className="group aspect-square hover:opacity-100 transition-all ease-in cursor-pointer flex flex-col hover:absolute items-center hover:z-50 duration-100 hover:scale-110 bg-white hover:shadow rounded-md">
+    <div
+      className={clsx(
+        "group aspect-square transition-all ease-in flex flex-col items-center",
+        partnerData.description.length &&
+          "hover:z-50 duration-100 hover:scale-110 bg-white hover:shadow rounded-md hover:absolute cursor-pointer",
+      )}
+    >
       <div className="h-5/6 flex items-center justify-center w-full max-w-100%">
         <Image
           src={partnerData.logo}
@@ -43,9 +50,11 @@ const PartnerItem = ({ partnerData }) => {
           {partnerData.title}
         </div>
       </div>
-      <div className="hidden group-hover:block text-[14px] text-center text-zinc-500 p-5 transition-all opacity-0 group-hover:opacity-100 ease-in">
-        {partnerData.description}
-      </div>
+      {partnerData.description.length ? (
+        <div className="hidden group-hover:block text-[12px] md:text-[14px] text-center text-zinc-500 px-5  pb-[16px] transition-all opacity-0 group-hover:opacity-100 ease-in">
+          {partnerData.description}
+        </div>
+      ) : null}
     </div>
   );
 };
