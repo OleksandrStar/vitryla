94fea985663665184d1c8037b522cffc439d412f
Oleksandr <oleksandr@Oleksandrs-MacBook-Pro.local>
2024-09-27 18:39:16 +0300

    feat: add footer

--- src/app/maker-space/page.js
+++ src/app/maker-space/page.js
@@ -1,12 +1,14 @@
 import React from "react";
 import MakerSpace from "@/features/maker-space";
 import MakerSpaceHeader from "@/features/maker-space/components/maker-space-header";
+import MakerSpaceFooter from "@/features/maker-space/components/maker-space-footer";
 
 const Page = () => {
   return (
     <div>
       <MakerSpaceHeader />
       <MakerSpace />
+      <MakerSpaceFooter />
     </div>
   );
 };
